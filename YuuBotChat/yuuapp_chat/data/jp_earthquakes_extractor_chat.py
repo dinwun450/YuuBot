82bc45d72342358c1804c54947bb745bba9ee6c9
dinwun450 <42019518+dinwun450@users.noreply.github.com>
2025-06-12 14:47:14 -0700

    Update and rename langchain_tools_jpearthquakes.py to jp_earthquakes_extractor_chat.py
    We're using the olde-way for handling the data.

---
+++ YuuBotChat/yuuapp_chat/data/jp_earthquakes_extractor_chat.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import pytz
+from datetime import datetime
+
+def JSTtoPST(the_time):
+    dt = datetime.strptime(the_time, '%Y年%m月%d日 %H時%M分ごろ')
+    timezone = pytz.timezone('Asia/Tokyo')
+    jst = timezone.localize(dt)
+    return jst.strftime('%Y-%m-%d %H:%M:%S')
+
+def get_earthquake_data():
+    r = requests.get("https://typhoon.yahoo.co.jp/weather/jp/earthquake/list/")
+    soup = BeautifulSoup(r.content, 'html.parser')
+    earthquakes_history = soup.find('table', class_="yjw_table")
+    content_of_earthquakes = earthquakes_history.find_all('tr')
+    content_of_earthquakes.pop(0)
+    quake_info = []
+    appendation_of_date = []
+    c = 0
+
+    for i in content_of_earthquakes:
+        quake_info.append(i.find_all("td"))
+
+    while c < len(quake_info):
+        date, time = JSTtoPST(quake_info[c][0].text).split(" ")[0], JSTtoPST(quake_info[c][0].text).split(" ")[1]
+        d = {}
+        
+        d["date"] = date
+        d["time"] = time
+        d["epicenter"] = quake_info[c][1].text
+        d["mag"] = float(quake_info[c][2].text)
+        d["intensity"] = quake_info[c][3].text
+        
+        if d["intensity"] == "---":
+            d["intensity"] = "---"
+
+        appendation_of_date.append(d)
+        c += 1
+
+    return appendation_of_date
