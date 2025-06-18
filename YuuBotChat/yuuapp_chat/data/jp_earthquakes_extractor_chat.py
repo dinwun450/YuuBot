97d51d53c579f2cdff9187a671df865eda06c460
dinwun450 <mightygboole1815@gmail.com>
2025-06-18 12:52:55 -0700

    Fix Dockerfile and Python Bugs

--- YuuBotChat/yuuapp_chat/data/jp_earthquakes_extractor_chat.py
+++ YuuBotChat/yuuapp_chat/data/jp_earthquakes_extractor_chat.py
@@ -29,7 +29,12 @@ def get_earthquake_data():
         d["date"] = date
         d["time"] = time
         d["epicenter"] = quake_info[c][1].text
-        d["mag"] = float(quake_info[c][2].text)
+
+        if d["mag"] == "---":
+            d["mag"] = None
+        else:
+            d["mag"] = float(quake_info[c][2].text)
+            
         d["intensity"] = quake_info[c][3].text
         
         if d["intensity"] == "---":
