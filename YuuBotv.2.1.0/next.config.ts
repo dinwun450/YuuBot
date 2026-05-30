583f96e25e1ef283ab47d9e8d9e178b22a0fc164
Dino Wun <42019518+dinwun450@users.noreply.github.com>
2026-05-30 00:21:30 -0700

    Add more files via upload

---
+++ YuuBotv.2.1.0/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+module.exports = {
+  allowedDevOrigins: ['192.168.68.62'],
+}
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
